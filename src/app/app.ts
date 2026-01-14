c1d80680579b5d752a1745da398aa4060cf31c81
Abhishek <abhishekdound@gmail.com>
2026-01-14 21:36:56 +0530

    Add task Modal Added

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from "./layouts/header/header";
 import { Footer } from "./layouts/footer/footer";
 import { RouterOutlet } from '@angular/router';
 import { Ui } from './ui-service/ui.service';
+import { AddTaskModal } from "./mainContent/home-content/bottom-actions/add-task-modal/add-task-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, RouterOutlet],
+  imports: [Header, Footer, RouterOutlet, AddTaskModal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
